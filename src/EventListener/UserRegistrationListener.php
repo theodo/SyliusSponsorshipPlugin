5d43bdb28074ab356e378db290168b42550aecf0
PILLOWPET <hadrien.lucas@gmail.com>
2023-04-15 00:47:51 +0200

    feat: create listener for user registration

---
+++ src/EventListener/UserRegistrationListener.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Acme\SyliusSponsorshipPlugin\EventListener;
+
+final class UserRegistrationListener {
+    public function createPromotionCoupon() {
+        $myVariable = true;
+        return $myVariable;
+    }
+
+}
