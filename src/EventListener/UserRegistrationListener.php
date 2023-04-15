5635bfebb9cd43d476a8dc8255c9295ef7fc96d5
PILLOWPET <hadrien.lucas@gmail.com>
2023-04-15 15:33:11 +0200

    feat: register service with promotion coupon code parameter

--- src/EventListener/UserRegistrationListener.php
+++ src/EventListener/UserRegistrationListener.php
@@ -4,8 +4,21 @@ declare(strict_types=1);
 
 namespace Acme\SyliusSponsorshipPlugin\EventListener;
 
+use Sylius\Component\Core\Repository\PromotionRepositoryInterface;
+use Sylius\Component\Promotion\Factory\PromotionCouponFactoryInterface;
+use Sylius\Component\Core\Model\PromotionInterface;
+
 final class UserRegistrationListener {
+    public function __construct(
+        private PromotionCouponFactoryInterface $promotionCouponFactory,
+        private PromotionRepositoryInterface $promotionRepository,
+        private string $promotionCode,
+    ) {
+    }
     public function createPromotionCoupon() {
+        /** @var PromotionInterface $promotion */
+        $promotion = $this->promotionRepository->findOneBy(['code' => $this->promotionCode]);
+        $coupon = $this->promotionCouponFactory->createForPromotion($promotion);
         $myVariable = true;
         return $myVariable;
     }
